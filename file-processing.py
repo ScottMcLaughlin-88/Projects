4e30552facf2ebde87b83971f90cfef22e9c23e5
scottmclaughlin-88 <semclaughlin2020@gmail.com>
2024-04-05 09:21:09 -0500

    updated code

--- file-processing.py
+++ file-processing.py
@@ -19,7 +19,7 @@ def array_practice(arr):
 
 def process_csv():
 #use underscores for file names
-    with open(r"C:\Users\scott\OneDrive\Python\Coding Projects\US Stock Market Dataset.csv") as csvfile:
+    with open(r"C:\Users\scott\OneDrive\Python\Projects\dataset.csv") as csvfile:
         lines=csv.reader(csvfile)
         data = list(csv.reader(csvfile))
         print("rows:", len(data))
@@ -48,6 +48,83 @@ def practice():
     d = find[1]
     e = l[1][1]
 
+def array_range(arr):#get smallest and largest elemnts of array
+    print(f"Before sorting: {arr}")
+    arr.sort()
+    print(arr)
+    small_list = arr[0]
+    print(f"Small list: {small_list}")
+    large_list = arr[-1]
+    large_list2 = arr[len(arr)-1]#Used for general programming languages
+    print(large_list, large_list2)
+
+def array_range_python(arr):
+    small = min(arr)
+    large = max(arr)
+    print(small,large)#Standard way to find small value is to initialize to a very large value, and then look for the smal l value.
+    #Standard way to find large value is to initialize to a very small value, then look for large value
+
+def array_range_for(arr):
+    small = float('inf')
+    large = float('-inf')
+    for val in arr:
+        if val < small:
+            small = val
+        if val > large:
+            large = val
+    return small, large
+
+#two types of arrays.  even and odd ones.
+def med_arr(arr):
+    arr.sort()
+    print(arr)
+    arr_length = len(arr)
+    if is_even(arr_length):
+#5/2 -> 2.5(leaves decimal), 5//2 2 (chops off all decimals)
+#[4,6,8,10], med[1,2]
+#half_len The length of the array // 2
+#close_len = half_len // 2
+#[4,6,8,10,13,15], med[2,3]
+#half_len = length of the // 2 = 6 // 2 = 3
+#close_len = half_len - 1
+        half_len  = arr_length // 2
+        close_len = half_len - 1
+        med_sum = arr[half_len] + arr[close_len]
+        return med_sum / 2
+    else:
+        med_index = arr_length // 2 #5 -> 2
+        return arr[med_index]
+
+
+        
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+    
+def div_practice():
+    odd = 5
+    even = 4
+    print(f"The even result is {even // 2}")
+    print(f"The odd result is {odd // 2}")    
+    print(f"The odd result is {odd / 2}")#single division is w/ float.  // int is not.
+
+def array_practice():
+    arr = [1,10,-10,2,3,4,5,-15,-3]
+    count = 0
+    total = 0
+    for val in arr:
+        if val >= 2:
+            total += val
+            count += 1
+    avg = total / count
+    print(f"Total is {total}")
+    print(f"The average is {avg}")
+    python_total = sum(arr)
+    python_average = python_total / len(arr)
+    print(f"The Python average is {python_average}")
+
 def review():
     a = [[1,5],
          [5,10]]
@@ -65,5 +142,14 @@ if __name__ == '__main__':
     # array_practice([1,56,3,324,89,100])
     # process_csv()
     # review()
-    twoFor()
+    arr = [-5, 3, -2, 10, -21]#makes array more dynamic because we can use it for any array
+    # array_range(arr)
+    # array_practice()
+    arr1 = [4, 2, 1, 3]
+    arr2 = [1, 3, 2, 4, 5]
+    med_2 = med_arr([4,6,8,10,13,15])
+    print(med_2)
+    # array_range_python(arr)
+    # small, large = array_range_for(arr)
+    # print(small,large)
     # process_csv(10)
